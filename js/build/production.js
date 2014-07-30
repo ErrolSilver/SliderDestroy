7507f783993fea2037bb39a62a2be0081eaf471b
Errol Silver <esilver@bostoninteractive.com>
2014-07-30 09:26:16 -0400

    basic functionality with bootstrap carousel

--- js/build/production.js
+++ js/build/production.js
@@ -2113,4 +2113,27 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
 
 }(jQuery);
 
-console.log("Hello");
+(function ($) {
+  console.log("Hello");
+
+  function sideBySide() {
+    if ($(window).width() > 768) {
+      $('#sliderDestroy').removeClass('slide');
+      $('.carousel-indicators').addClass('hidden');
+      $('.carousel-control').addClass('hidden');
+    }
+  }
+
+  $(window).resize(function () {
+    console.log ($(window).width());
+    if ($(window).width() < 768) {
+      $('#sliderDestroy').addClass('slide');
+      $('.carousel-indicators').removeClass('hidden');
+      $('.carousel-control').removeClass('hidden');
+    } else {
+      sideBySide();
+    }
+  });
+
+sideBySide();
+}(jQuery));
--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+(function ($) {
+  console.log("Hello");
+
+  function sideBySide() {
+    if ($(window).width() > 768) {
+      $('#sliderDestroy').removeClass('slide');
+      $('.carousel-indicators').addClass('hidden');
+      $('.carousel-control').addClass('hidden');
+    }
+  }
+
+  $(window).resize(function () {
+    console.log ($(window).width());
+    if ($(window).width() < 768) {
+      $('#sliderDestroy').addClass('slide');
+      $('.carousel-indicators').removeClass('hidden');
+      $('.carousel-control').removeClass('hidden');
+    } else {
+      sideBySide();
+    }
+  });
+
+sideBySide();
+}(jQuery));
