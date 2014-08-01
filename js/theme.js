f64b6f9549a99b45c81b369d913cfe8314f54e82
Errol Silver <esilver@bostoninteractive.com>
2014-08-01 11:10:16 -0400

    implementing more than 3 items

--- js/theme.js
+++ js/theme.js
@@ -1,45 +1,55 @@
 (function ($) {
   var $carousels = $('.carousel'),
-    $carItems = $('.carousel-inner')
+    $carPlusThree = $('.plusThree'),
+    $carItems = $('.carousel-inner'),
     $carIndicate = $('.carousel-indicators'),
     $carControls = $('.carousel-control');
 
-    console.log($carItems.children('.item').length);
+
   function sideBySide() {
-    if ($(window).width() > 768 && $carousels.children('.item').length < 3) {
+
+    // Check carousels for their number of slides
+    $carItems.each(function () {
+      var children = $(this).find('.item').length,
+        $parent = $(this).parent();
+
+      // Add a class to carousels with more than 3 slides
+      if (children > 3) {
+        $parent.addClass('plusThree')
+      }
+    });
+
+    if ($(window).width() > 768) {
+
+      // If the window is larger than extra small screens, remove bootstrap carousel controls 
       $carousels.removeClass('slide');
       $carIndicate.addClass('hidden');
       $carControls.addClass('hidden');
-      $carItems.children('.item').addClass('threeSlides');
+
+      // Add class setting new slide width to display side-by-side
+      $carItems.children('.item').addClass('col-sm-4 active');
+
+      // Prevent slideshow from attempting to run if above breakpoint
       $carousels.carousel('pause');
+
     }
   }
 
   function checkSlides() {
-    if ($carItems.children('.item').length > 3) {
-      $(this).children('.item').each(function () {
-        var next = $(this).next();
-        if (!next.length) {
-          next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
 
-        for (var i = 0; i < 2; i++) {
-          next = next.next();
-          if (!next.length) {
-            next = $(this).siblings(':first');
-          }
-          next.children(':first-child').clone().appendTo($(this));
-        }
-      });
-    }
   }
 
   $(window).resize(function () {
     if ($(window).width() < 768) {
+
+      // Restore bootstrap controls if extra small screen size
       $carousels.addClass('slide');
       $carIndicate.removeClass('hidden');
       $carControls.removeClass('hidden');
+      $carousels.removeClass('plusThree');
+      $carItems.children('.item').removeClass('col-sm-4 active');
+      $carItems.children('.item:first-child').addClass('active');
+
       $carousels.carousel('cycle');
     } else {
       sideBySide();
