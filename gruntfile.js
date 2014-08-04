b82c2b280da7b11208acb8476f2a1da38535fa55
Errol Silver <esilver@bostoninteractive.com>
2014-08-04 10:50:34 -0400

    removing unused plugins

--- gruntfile.js
+++ gruntfile.js
@@ -4,21 +4,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    concat: {
-      dist: {
-        src: [
-          'js/*.js', // All JS in the js folder
-        ],
-        dest: 'js/build/production.js',
-      }
-    }, // End Concat
-
-    uglify: {
-      build: {
-        src: 'js/build/production.js',
-        dest: 'js/build/production.min.js'
-      }
-    }, // End Uglify
 
     compass: {
       dist: {
@@ -35,7 +20,6 @@ module.exports = function(grunt) {
       },
       scripts: {
         files: ['js/*.js'],
-        tasks: ['concat', 'uglify'],
         options: {
           spawn: false,
         },
@@ -55,13 +39,10 @@ module.exports = function(grunt) {
   }); // End Configuration
 
   // Plugin references
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Task registration
-  grunt.registerTask('default', ['concat', 'uglify', /*'imagemin',*/ 'watch']);
+  grunt.registerTask('default', ['watch']);
 
 };
